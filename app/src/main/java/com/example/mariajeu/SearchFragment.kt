deb1be9e415eeec36c7f84395f5ff61f9bac1035
eccho03 <choeunchae0828@gmail.com>
2024-01-20 13:27:23 +0900

    리스트 뷰 작업 중

--- app/src/main/java/com/example/mariajeu/SearchFragment.kt
+++ app/src/main/java/com/example/mariajeu/SearchFragment.kt
@@ -1,7 +1,25 @@
 package com.example.mariajeu
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.mariajeu.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+    lateinit var binding: FragmentSearchBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
 }
